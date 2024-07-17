0eab58301dde53afc2136b09e3407226c41a2529
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-07-17 15:20:38 -0300

    formsReact

---
+++ reactForms/src/App.jsx
@@ -0,0 +1,16 @@
+import MyForm from './components/MyForm'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>Forms</h2>
+      <MyForm/>
+    </>
+  )
+}
+
+export default App
