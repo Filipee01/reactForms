b5eff7fdf3a9af65480962b4264b886613e6c32a
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-07-17 15:40:19 -0300

    useState

--- reactForms/src/components/MyForm.jsx
+++ reactForms/src/components/MyForm.jsx
@@ -1,18 +1,30 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm =  () => {
+    // Gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name)
+    console.log(email)
+
     return (    
         <div>
             {/* Criação de form */}
             <form >
                 <div>
                     <label htmlFor="name">Nome</label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" />
+                    <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
                 </div>
                 {/* Label envolvendo Input */}
                 <label>
                     <span>E-mail</span>
-                    <input type="email" name="email" placeHolder="Digite o seu E-mail" />
+                    <input type="email" name="email" placeholder="Digite o seu E-mail" onChange={(e) => setEmail(e.target.value)}/>
                 </label>
                 <input type="submit"  value="Enviar"/>
             </form>
