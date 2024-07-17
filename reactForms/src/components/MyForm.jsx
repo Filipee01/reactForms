4750dcf82fb2b9e4ab6436eebd17c2cbd05c69f8
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-07-17 15:25:32 -0300

    labelInput

--- reactForms/src/components/MyForm.jsx
+++ reactForms/src/components/MyForm.jsx
@@ -1,7 +1,7 @@
 import './MyForm.css'
 
 const MyForm =  () => {
-    return (
+    return (    
         <div>
             {/* Criação de form */}
             <form >
@@ -9,6 +9,11 @@ const MyForm =  () => {
                     <label htmlFor="name">Nome</label>
                     <input type="text" name="name" placeholder="Digite o seu nome" />
                 </div>
+                {/* Label envolvendo Input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email" placeHolder="Digite o seu E-mail" />
+                </label>
                 <input type="submit"  value="Enviar"/>
             </form>
         </div>
